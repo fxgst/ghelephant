06027a020a349c0bf2f95fe5eca99689c583fd8c
Elias Datler <eliasdatler@me.com>
2022-12-16 03:30:03 +0100

    new function name to show off speed

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from scheduler import Scheduler
 
 def main():
     logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
-    scheduler = Scheduler(start_year=2022, start_month=12, start_day=1, end_year=2022, end_month=12, end_day=1)
-    scheduler.copy_insert()
+    scheduler = Scheduler(start_year=2022, start_month=1, start_day=1, end_year=2022, end_month=12, end_day=15)
+    scheduler.fast_insert()
 
 
 if __name__ == '__main__':
