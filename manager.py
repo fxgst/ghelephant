dde764ac39ce72f486f737551fa2cb3284f7b4bd
Elias Datler <eliasdatler@me.com>
2022-12-22 01:00:25 +0100

    fix decompression stall, terminate nicer

--- manager.py
+++ manager.py
@@ -1,5 +1,4 @@
 import requests
-import gzip
 import os
 import logging
 import datetime
@@ -18,10 +17,7 @@ class Manager:
         self.end_year = end_year
         self.end_month = end_month
         self.end_day = end_day
-        dates_to_download = self.__dates_to_download()
-        self.dates_to_download = iter(dates_to_download)
-        self.last_hour = dates_to_download[-1]
-        self.last_day = self.last_hour[:-2]
+        self.dates_to_download = self.__dates_to_download()
         self.downloadedQueue = Queue(maxsize=10)
         self.decompressedQueue = Queue(maxsize=10)
         self.written_queue = Queue(maxsize=3)
@@ -30,13 +26,13 @@ class Manager:
         while date := next(self.dates_to_download, None):
             self.download_json(date)
             self.downloadedQueue.put(date)
+        self.downloadedQueue.put(None)
 
     def run_decompress(self):
         while date := self.downloadedQueue.get():
             self.decompress_json(date)
             self.decompressedQueue.put(date)
-            if date == self.last_hour:
-                break
+        self.decompressedQueue.put(None)
 
     def run_write_csvs(self):
         with DatabaseLink() as db:
@@ -57,22 +53,19 @@ class Manager:
             if hour == '0':
                 converter.writers.close()  # type: ignore
                 self.written_queue.put(day)
-                if date == self.last_hour:
-                    break
+        self.written_queue.put(None)
 
         with DatabaseLink() as db:
             db.add_primary_keys()
 
     def run_copy_into_database(self):
         while day := self.written_queue.get():
-            logging.info('Inserting csvs into database')
+            logging.info(f'Copying {day} into database')
             with DatabaseLink() as db:
                 db.insert_csvs_into_db(day)
             self.remove_inserted_csvs(day)
-            if day == self.last_day:
-                break
 
-    def __dates_to_download(self) -> list:
+    def __dates_to_download(self):
         dates_to_download = []
         start_date = datetime.date(self.start_year, self.start_month, self.start_day)
         end_date = datetime.date(self.end_year, self.end_month, self.end_day)
@@ -82,14 +75,13 @@ class Manager:
             for h in range(0, 24):
                 dates_to_download.append(f'{day.year}-{str(day.month).zfill(2)}-{str(day.day).zfill(2)}-{h}')
         dates_to_download.reverse()
-        return dates_to_download
+        return iter(dates_to_download)
 
     @staticmethod
     def download_json(date_to_download):
         path = f'{data_path}/{date_to_download}'
         if os.path.isfile(f'{path}.json'):
             return
-        # download compressed file
         logging.info(f'Downloading {date_to_download}')
         response = requests.get(f'https://data.gharchive.org/{date_to_download}.json.gz')
         with open(f'{path}.json.gz', 'wb') as f:
@@ -100,11 +92,8 @@ class Manager:
         path = f'{data_path}/{date_to_download}'
         if os.path.isfile(f'{path}.json'):
             return
-        # decompress file, delete compressed file
         logging.info(f'Decompressing {date_to_download}')
-        with gzip.open(f'{path}.json.gz', 'rb') as compressed, open(f'{path}.json', 'wb') as uncompressed:
-            uncompressed.write(compressed.read())
-        os.remove(f'{path}.json.gz')
+        os.system(f'gunzip {path}.json.gz')
 
     @staticmethod
     def remove_json(date_to_download):
