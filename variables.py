9e8923e084ce1cd742c7ea3d02ba357dc6691579
Elias Datler <eliasdatler@me.com>
2023-04-06 19:57:31 +0200

    change gsed to default sed

--- variables.py
+++ variables.py
@@ -4,4 +4,4 @@ database_password = ''
 database_host = 'localhost'
 database_port = '5432'
 data_path = ''
-sed_name = 'gsed'  # gsed for mac, sed for linux
+sed_name = 'sed'  # gsed for mac, sed for linux
